0d21cb2844c566edb140e4cdc92d6d68fb71ded2
cocodevweb <83016254+cocodevweb@users.noreply.github.com>
2022-04-01 01:27:44 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,13 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports = {
   name: "ping",
   run: (message, client) => {
-    message.channel.send("Pong !")
+    const initialEmbed = new MessageEmbed().setDescription("Pinping...").setColor("RANDOM")
+    message.channel.send({ embeds: [initialEmbed] }).then(msg => {
+      const ping = message.createdTimestamp - message.createdTimestamp
+      const pingEmbed = new MessageEmbed().setDescription(`Ping : ${ping}ms`).setColor("RANDOM")
+      msg.edit({embeds: [pingEmbed]})
+    })
   }
 }
