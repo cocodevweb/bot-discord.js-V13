153c811662d006879d1aeb4ed7998c7c6e27d8f0
cocodevweb <83016254+cocodevweb@users.noreply.github.com>
2022-04-01 01:15:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ client.on('ready', () => {
   console.log("Le bot est en ligne !")
 })
 
+client.on("messageCreate", message => {
+  if (message.author.bot) return
+  const prefix = process.env.PREFIX;
+  if (!message.content.startsWith(prefix)) return
+  const args = message.content.split(" ");
+  const commandName = args.shift().slice(prefix.length)
+  if(commandName == "ping") {
+    message.channel.send("Pong !")
+  }
+})
+
 client.login(process.env.TOKEN)
